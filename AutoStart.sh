4a3106a6ec69a461e1ee3e5e3e46ccb66edcee25
Henry Kotze <henryykotze@gmail.com>
2017-06-18 22:27:30 +0200

    Give subshell access to parent variables
    Setting the PID's and TIME local variables of parent process to ENV variables, so that the child process can access them

--- AutoStart.sh
+++ AutoStart.sh
@@ -4,9 +4,9 @@ PROGRAM_NAME="firefox" # name of program to restart if it crashes. example: main
 PATH_OF_PROGRAM=""
 password=""
 
-while sleep 30; do # waits for program to start after restart, thus ensure sleep (x) is long enough
+while sleep 1; do # waits for program to start after restart, thus ensure sleep (x) is long enough
   PROG_INFO=`ps -e | grep ${PROGRAM_NAME}`
-  echo $PROG_INFO
+  echo $PID_INFO
 
   array=($PROG_INFO)
 
@@ -15,33 +15,50 @@ while sleep 30; do # waits for program to start after restart, thus ensure sleep
   PID_NAME=${array[3]}
 
 
-  echo $PID # Process Number
-  echo $TIME # Time of how long the program has been active
-  echo $PID_NAME #Process name according top, should be the same as $PROGRAM_NAME
+  echo "PID: " $PID # Process Number
+  echo "TIME: " $TIME # Time of how long the program has been active
+  echo "PID_NAME: " $PID_NAME #Process name according top, should be the same as $PROGRAM_NAME
 
 
-  while sleep 1; do #check every (x) seconds if the program has crash
 
+  export PID
+  export TIME
+
+  { while sleep 1; do #check every (x) seconds if the program has crash
+    echo "in Checker Loop"
+    echo $PROGRAM_NAME
     PID_INFO=`ps -e | grep ${PROGRAM_NAME}`
 
-    array=($PROG_INFO)
+    array=($PID_INFO)
 
     NEW_PID=${array[0]}
     NEW_TIME=${array[2]}
     NEW_PID_NAME=${array[3]}
 
-    if PID_NAME != NEW_PID_NAME; then #if PID's differ we will restart the app
-      sudo $PATH_OF_PROGRAM/$PROGRAM_NAME &
-      expect "password: "
-      send "$password"
-      break;
-    elif TIME == TIME; then #if TIME is equal, program has hanged, and thus we must restart it
-      sudo $PATH_OF_PROGRAM/$PROGRAM_NAME &
-      expect "password: "
-      send "$password"
-      break;
+    echo "NEW_PID: " $NEW_PID
+    echo "PID: " $PID
+    sleep 2
+
+    echo "TIME: " $TIME
+    echo "NEW_TIME: " $NEW_TIME
+
+
+    if [ "$PID" != "$NEW_PID" ]; then #if PID's differ we will restart the app
+      echo "PID HAS CHANGED"
+      # sudo $PATH_OF_PROGRAM/$PROGRAM_NAME &
+      # expect "password: "
+      # send "$password"
+      break
+    elif [ $TIME == $NEW_TIME ]; then #if TIME is equal, program has hanged, and thus we must restart it
+      echo "TIME IS CONSTANT"
+      # sudo $PATH_OF_PROGRAM/$PROGRAM_NAME &
+      # expect "password: "
+      # send "$password"
+      # break
     fi
-  done;
+    echo "sleeping"
+    sleep 1
+  done }
 
 done
 
